4459a1ea128526b898af10c8fa2eee7d0384fe8f
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2019-09-04 07:41:27 +0500

    Remove unsued code & intl module

--- util.js
+++ util.js
@@ -1,18 +1,5 @@
 'use strict';
 
-const Intl = require('intl');
-
-const formatter = new Intl.DateTimeFormat('ru', {
-    timeZone: 'UTC',
-    hour12: false,
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-});
-exports.DtString = dt => `${formatter.format(new Date(dt))} UTC`;
-
 exports.GetFirstUserMention = str => {
     const match = str.match(/<@!?([0-9]+)>/);
     if(match)
